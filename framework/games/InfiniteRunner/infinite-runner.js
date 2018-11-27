66e1502fd7998f9def2917f102f31b0fcb3796b5
Jason Zareski <zareskjj@dukes.jmu.edu>
2018-11-27 13:37:00 -0500

    Collisions with obstacles

--- framework/games/InfiniteRunner/infinite-runner.js
+++ framework/games/InfiniteRunner/infinite-runner.js
@@ -14,14 +14,22 @@ function gameMessageHandler(msg) {
 
 }
 
-let mass = 8;
+let entityLimit = 18;
+let baseSpeed = -40;
+
+let playerWidth = windowWidth * 0.05;
+let playerHeight = windowHeight * 0.1;
+let playerOffset = 50;
+
+let mass = 0.5;
+let gravity = createVector(0, 30 * mass);
+
+let jumpCount = 0;
+let radius = playerWidth / 2;
 
 var position;
 var speed; // Used for jumping
 var accel; // Gravity simulation
-let playerWidth = windowWidth * 0.05;
-let playerHeight = windowHeight * 0.1;
-let gravity = createVector(0, mass);
 
 let resetY = windowHeight - playerHeight - 30;
 
@@ -29,11 +37,16 @@ function newPlayer() {
     accel = createVector(0, 0);
     speed = createVector(0, 0);
     position = createVector(windowWidth * 0.35,
-        windowHeight - playerHeight - 30);
+        windowHeight - playerHeight - playerOffset);
+    jumpCount = 0;
 }
 
 function jump() {
-    accel = createVector(0, -6.5);
+    if (jumpCount < 2) {
+        jumpCount += 1;
+        var jumpForce = jumpCount == 1 ? -90 : -80;
+        accel = createVector(0, jumpForce);
+    }
 }
 
 function applyForce(force) {
@@ -49,31 +62,59 @@ function update() {
         position.y = resetY;
         speed = createVector(0,0);
         accel = createVector(0,0);
+        jumpCount = 0;
     }
     position.x = windowWidth * 0.35;
+
+    //Move rewards & obstacles
+    let ospd = createVector(baseSpeed + (-0.01 * score), 0);
+    for (let i = 0; i < envObst.length; i++)
+    {
+        let v = createVector(envObst[i][0], envObst[i][1]);
+        v.add(ospd);
+        if (v.x <= 0)
+        {
+            envObst.splice(i, 1);
+        } else {
+            envObst[i][0] = v.x;
+            envObst[i][1] = v.y;
+        }
+    }
+
+
     display();
 }
 
 function display() {
-    rectMode(CENTER);
+    rectMode(CORNER);
     stroke(69, 0, 132);
     strokeWeight(1);
     fill(203, 182, 119);
     rect(position.x, position.y, playerWidth, playerHeight, 5);
+
+    strokeWeight(3);
+    let len = envObst.length;
+    for (let i=0;i<len;i++) {
+        fill(randomInt(225, 1), randomInt(100, 50), randomInt(100,50));
+        ellipse(envObst[i][0], envObst[i][1], radius, radius);
+    }
+
 }
 
-var environmentObstacles; // Array of obstacles
+var envObst; // Array of obstacles
 var environmentRewards; // Array of rewards
+var delay;
 
 var score = 0;
 var highScore = 0;
 var gameSpeed = 10;
 
 function newGame() {
-    environmentObstacles = [];
+    envObst = [];
     environmentRewards = [];
     score = 0;
-    gameSpeed = 10;
+    gameSpeed = 20; //TODO was 10
+    delay = 0;
     newPlayer();
 }
 
@@ -84,6 +125,7 @@ function setup() {
     //Setup New Game
     frameRate(gameSpeed);
     newGame();
+    console.log("PW,PH: %d, %d", playerWidth, playerHeight);
 }
 
 function onClick(elementID, id) {
@@ -93,7 +135,44 @@ function onClick(elementID, id) {
 }
 
 function checkCollisions() {
+    let x = position.x;
+    let y = position.y;
 
+    for (let i = 0; i < envObst.length; i++)
+    {
+        let ox = envObst[i][0] - x;
+        let oy = envObst[i][1] - y;
+        if (ox >= 0 && oy >= 0 && ox < playerWidth + radius && oy < playerHeight + radius) {
+            envObst.splice(i, 1);
+            endGame();
+        }
+    }
+
+}
+
+//Player never actually moves, obstacles approach from right
+function generateEnvironment() {
+    //Depends on score, generate obstacles/rewards
+    if (delay <= 0) {
+
+        let objX = windowWidth * 1.1;
+        let objY = randomInt(playerHeight * 2, windowHeight - (2*playerHeight));
+
+        if (Math.random() >= 0.40) {
+            // obstacle (more probable to spawn)
+            envObst.push( [objX, objY] );
+            if (Math.random() >= 0.70) {
+                objX = randomInt(30, objX);
+                objY = randomInt(40, objY/3);
+                envObst.push( [objX, objY] );
+            }
+        } else {
+            // reward
+            environmentRewards.push( [objX, objY] );
+        }
+        delay = 30 - (score * 0.01);
+    }
+    delay -= 1;
 }
 
 function draw() {
@@ -101,7 +180,7 @@ function draw() {
     drawScoreboard();
     changeSpeed();
 
-    update();
+    update(); // Player update
 
     displayGround();
     generateEnvironment();
@@ -129,25 +208,23 @@ function drawScoreboard() {
 
 /*Change speed based on score*/
 function changeSpeed() {
-    if (score >= 5 && score < 10) {
-        gameSpeed = 10;
-    } else if (score > 10 && score < 15) {
-        gameSpeed = 12;
-    } else if (score >= 15) {
-        gameSpeed = 13;
-    }
     frameRate(gameSpeed);
 }
 
 function displayGround() {
-    rectMode(CENTER);
+    rectMode(CORNER);
     stroke(0, 0, 0);
     strokeWeight(2);
     fill(0, 0, 0);
-    rect(0, windowHeight - 40, windowWidth * 2, 45, 0);
+    rect(0, windowHeight - playerOffset + 22, windowWidth * 2, 100);
 }
 
-//Player never actually moves, obstacles approach from right
-function generateEnvironment() {
-    //Depends on score, generate obstacles/rewards
+function randomInt(range, offset) {
+    return Math.floor(Math.random() * range) + offset;
+}
+
+function endGame() {
+    highScore = highScore < score ? score : highScore;
+    score = 0;
+
 }
